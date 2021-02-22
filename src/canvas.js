c8e94da64fdc7b70f54c36107262395c37a51c1b
Robert Bruce Gifford <timeonator@gmail.com>
2021-02-21 18:56:40 -0800

    drawImage with translation

--- src/canvas.js
+++ src/canvas.js
@@ -3,18 +3,20 @@ import React, { useRef, useEffect } from "react";
 const Canvas = (props) => {
   const canvasRef = useRef(null);
 
-  const draw = () => {};
-
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    let frameCount = 0;
     let animationFrameId;
 
+    const { canvasWidth, canvasHeight } = canvas.getBoundingClientRect();
+    const draw = (context) => {
+      var img = new Image(); // Create new img element
+      img.src = "7debaf0c-a396-4a57-a000-b1e418c4cb7c.png"; // Set source path
+      context.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+    };
     //Our draw came here
     const render = () => {
-      frameCount++;
-      draw(context, frameCount);
+      draw(context);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
@@ -22,7 +24,7 @@ const Canvas = (props) => {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [draw]);
+  }, []);
 
   return <canvas ref={canvasRef} {...props} />;
 };

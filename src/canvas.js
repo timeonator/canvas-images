db6931afff70a1e9161fb819671d958446da0904
Robert Bruce Gifford <timeonator@gmail.com>
2021-02-21 22:45:51 -0800

    removed console.log

--- src/canvas.js
+++ src/canvas.js
@@ -10,17 +10,18 @@ const Canvas = (props) => {
 
     const { width, height, ...rest } = canvas.getBoundingClientRect();
 
-    console.log(width, height);
     const draw = (context) => {
       var img = new Image(); // Create new img element
       img.src = "7debaf0c-a396-4a57-a000-b1e418c4cb7c.png"; // Set source path
       context.drawImage(img, width / 4, height / 4, width / 2, height / 2);
     };
+
     //Our draw came here
     const render = () => {
       draw(context);
       animationFrameId = window.requestAnimationFrame(render);
     };
+
     render();
 
     return () => {
